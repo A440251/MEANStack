1758b9dd4f20a055efecaed8a6b388c50a3899c8
A440251 <A440251@gmail.com>
2015-07-29 17:02:52 -0400

    Add total votes feature

--- app.js
+++ app.js
@@ -13,23 +13,26 @@ function($scope){
 			title: $scope.title,
 			link: $scope.link,
 			upvotes: 0,
-			downvotes: 0
+			downvotes: 0,
+			totalvotes: 0
 		});
 		$scope.title = '';
 		$scope.link = '';
 	};
 	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
+		post.totalvotes += 1;
 	};
 	$scope.incrementDownvotes = function(post) {
 		post.downvotes += 1;
+		post.totalvotes -= 1;
 	};
 	$scope.posts = [
-		{title: 'post 1', upvotes: 5, downvotes: 0},
-		{title: 'post 2', upvotes: 2, downvotes: 0},
-		{title: 'post 3', upvotes: 15, downvotes: 0},
-		{title: 'post 4', upvotes: 9, downvotes: 0},
-		{title: 'post 5', upvotes: 4, downvotes: 0}
+		{title: 'The first post', upvotes: 5, downvotes: 0, totalvotes: 5},
+		{title: 'Another post', upvotes: 2, downvotes: 0, totalvotes: 2},
+		{title: 'The third popular one', upvotes: 15, downvotes: 0, totalvotes: 15},
+		{title: 'Post four', upvotes: 9, downvotes: 0, totalvotes: 9},
+		{title: 'Post 5', upvotes: 4, downvotes: 0, totalvotes: 4}
 	];
 	
 }
